46c2e0c7d0ad64674da2847e3cc31850e29da74f
melikakh2024 <melikakh2024@gmail.com>
2026-08-27 16:35:30 +0200

    update country

--- T-DBT/pip_db_project/ghcn_weather/models/staging/country.sql
+++ T-DBT/pip_db_project/ghcn_weather/models/staging/country.sql
@@ -1,13 +1,15 @@
-with
-source as (
-    select * from  {{ source ('gchn_source', 'country') }}
+with source as (
+    select * from {{ source('gchn_source', 'country') }}
 ),
 
 renamed as (
     select
-        cast( id as string) as id,
-        country  as country
+        cast(id as string) as id,
+        cast(country as string) as country
     from source
 )
-select * from renamed
 
+select
+    id,
+    country
+from renamed
